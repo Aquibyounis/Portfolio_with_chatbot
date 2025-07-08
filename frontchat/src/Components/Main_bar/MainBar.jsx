a37577073b2ed7df201f543bc74a71657c18441d
Aquib younis <aquibyounis1@gmail.com>
2025-07-08 23:47:04 +0530

    updates

--- frontchat/src/Components/Main_bar/MainBar.jsx
+++ frontchat/src/Components/Main_bar/MainBar.jsx
@@ -14,6 +14,7 @@ const MainBar = () => {
                     <p>You are just a click away from using my AI assistant BOT <i style={{"fontSize":"1.5em","color":"#065cb8c5"}} class="fa-solid fa-location-arrow"></i></p>
                 </span>
                 <h3 className='ai'>Aspiring AI Engineer <i class="fa-solid fa-brain"></i></h3>
+                <a className='resume' href='https://drive.google.com/file/d/13Dic9YudvAwR1GFSogC_bfOZZFsy9jmg/view?usp=sharing'>RESUME</a>
             </div>
             <div className="right">
                 <img src={profile} alt="" className='img_p'/>
