1487c84e899384ab324a6f0a81496cc2d08e7c42
Aquib younis <aquibyounis1@gmail.com>
2025-08-03 00:09:46 +0530

    New pfp

--- frontchat/src/Components/Main_bar/MainBar.jsx
+++ frontchat/src/Components/Main_bar/MainBar.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
 import "./MainBar.css"
-import profile from "../../assets/pfp2.png"
+import profile from "../../assets/jojo2.png"
 
 const MainBar = () => {
   return (
     <div className='mainbar'>
         <div className="inner_main">
             <div className="middle">
-                <h1 className='h1'>PULA AQUIB YOUNIS</h1>
+                <h1 className='h1'>PULA <bold className='bold'>AQUIB</bold> YOUNIS</h1>
                 <span className='ai_bot'>
                     <p>Welcome to my portfolio...</p>
                     <p>If you have any doubt about me...</p>
-                    <p>You are just a click away from using my AI assistant <strong style={{color:"lightgreen"}}>Peanut</strong> <i style={{"fontSize":"1.5em","color":"#1C73A9FF"}} class="fa-solid fa-location-arrow"></i></p>
+                    <p>You are just a <bold style={{fontWeight:"800",color:'#BB65ff'}}>click</bold> away from using my AI assistant <strong style={{color:"lightgreen"}}>Peanut</strong> <i style={{"fontSize":"1.5em","color":"#1C73A9FF"}} class="fa-solid fa-location-arrow"></i></p>
                 </span>
                 <h3 className='ai'>Aspiring AI Engineer <i class="fa-solid fa-brain"></i></h3>
                 <a className='resume' href='https://drive.google.com/file/d/13Dic9YudvAwR1GFSogC_bfOZZFsy9jmg/view?usp=sharing'>RESUME</a>
             </div>
             <div className="right">
-                <img src={profile} alt="" className='img_p'/>
+                <div class="img-wrapper">
+                    <img src={profile} alt='' class="img_p" />
+                </div>
+
             </div>
         </div>
     </div>
--- frontchat/src/Components/Navbar/Navbar.jsx
+++ frontchat/src/Components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
             <NavLink className={({ isActive }) => isActive ? 'Link active' : 'Link'} to="/">Home <i className="fa-solid fa-house"></i></NavLink>
             <NavLink className={({ isActive }) => isActive ? 'Link active' : 'Link'} to="/certifications">Certifications <i className="fa-regular fa-folder-open"></i></NavLink>
             <NavLink className={({ isActive }) => isActive ? 'Link active' : 'Link'} to="/projects">Projects <i className="fa-solid fa-laptop-code"></i></NavLink>
-            <NavLink className={({ isActive }) => isActive ? 'Link active' : 'Link'} to="/assistant">AI Assistant <i style={{ fontSize: "0.8em" }} className="fa-solid fa-chevron-down"></i></NavLink>
+            <NavLink className={({ isActive }) => isActive ? 'Link active' : 'Link'} to="/assistant">Peanut <i style={{ fontSize: "0.8em" }} className="fa-solid fa-chevron-down"></i></NavLink>
         </span>
     </div>
   )
