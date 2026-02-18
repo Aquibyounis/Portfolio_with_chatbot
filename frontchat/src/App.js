fa622d8043589c1590f16cb05520cb0f7f140647
Aquib younis <aquibyounis1@gmail.com>
2026-02-18 15:34:00 +0530

    fixed loading animation time

--- frontchat/src/App.js
+++ frontchat/src/App.js
@@ -18,7 +18,7 @@ function App() {
     // show loader for 2 seconds
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 0);
+    }, 3000);
     return () => clearTimeout(timer);
   }, []);
 
